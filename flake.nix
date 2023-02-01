392ce2a859cd1816e72f4e9771f2897a3bb3525a
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-01 12:29:05 -0800

    downgrade nixpkgs with ghc-8.10.7

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "eventlog-live";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
@@ -14,12 +14,14 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      hsPkgs = pkgs.haskell.packages.ghc944;
+      hsPkgs = pkgs.haskell.packages.ghc8107;
       hsenv = hsPkgs.ghcWithPackages (p: []);
       shell = pkgs.mkShell {
         buildInputs = [
           hsenv
           pkgs.alejandra # nix expression formatter
+          pkgs.cabal-install
+          pkgs.zlib
         ];
       };
     in {
